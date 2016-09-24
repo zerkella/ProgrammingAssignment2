7a04adcce1f736bb7c0837eba3c98bf6dd0bb8d2
Andrey Tserkus <atserkus@rubiconproject.com>
2016-09-24 11:23:59 -0700

    Implemented the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Special matrix functions to work with matrices and their cached inversions 
 
-## Write a short comment describing this function
+# Create an "object" which contains a matrix "m" and its inverse "inverse"
+# Expose the API: set(), get(), setInverse, getInverse()
+makeCacheMatrix <- function(m = matrix()) {
+    inverse <- NULL;
 
-makeCacheMatrix <- function(x = matrix()) {
-
+    set <- function (value) {
+        m <<- value;
+        inverse <<- NULL;
+    };
+    
+    get <- function () {
+        m;
+    }
+    
+    setInverse <- function (value) {
+        inverse <<- value;
+    };
+    
+    getInverse <- function () inverse;
+    
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse);
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Calculate the inverse of CacheMatrix "m", but try to used cached inverse value, if any.
+cacheSolve <- function(m, ...) {
+    result <- m$getInverse();
+    if (is.null(result)) {
+        message("calculating inverse");
+        result <- solve(m$get());
+        m$setInverse(result);
+    }
+    result;
 }
